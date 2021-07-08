fe126879b8628d3f9dbb7ea49b29bd630e1145e3
Brett Comber <comber.brett@gmail.com>
2021-07-08 22:25:13 +1000

    Copying changes made to ./pocketsmith/pocketsmith_client.py upstream to ./_overrides/pocketsmith_client.py. When generate-pocketsmith-api.sh is run, it copies ./_overrides/pocketsmith_client.py to ./pocketsmith/.

--- _overrides/pocketsmith_client.py
+++ _overrides/pocketsmith_client.py
@@ -8,8 +8,10 @@ class PocketsmithClient:
         self.api_client = pocketsmith.ApiClient(configuration)
 
         self.accounts = pocketsmith.AccountsApi(self.api_client)
+        self.attachments = pocketsmith.AttachmentsApi(self.api_client)
         self.budgeting = pocketsmith.BudgetingApi(self.api_client)
         self.categories = pocketsmith.CategoriesApi(self.api_client)
+        self.category_rules = pocketsmith.CategoryRulesApi(self.api_client)                
         self.institutions = pocketsmith.InstitutionsApi(self.api_client)
         self.transaction_accounts = pocketsmith.TransactionAccountsApi(self.api_client)
         self.transactions = pocketsmith.TransactionsApi(self.api_client)
